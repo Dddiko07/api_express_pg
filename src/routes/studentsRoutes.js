a908267f8c299db7667df19c49153f1c168e218b
Dddiko07 <159420772+Dddiko07@users.noreply.github.com>
2025-10-28 19:35:14 +0700

    Update studentsRoutes.js

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -8,20 +8,9 @@ import {
 } from '../controllers/studentsController.js';
 
 const router = express.Router();
-
-// GET semua mahasiswa
 router.get('/', getStudents);
-
-// GET mahasiswa berdasarkan ID
 router.get('/:id', getStudent);
-
-// POST tambah mahasiswa baru
 router.post('/', addStudent);
-
-// PUT ubah data mahasiswa
 router.put('/:id', editStudent);
-
-// DELETE hapus mahasiswa
 router.delete('/:id', removeStudent);
-
 export default router;
